ea8b25d4d36097e46bf53b612bea98300cbfc81d
Kooroshh <mr.oplus@yahoo.fr>
2020-03-03 20:51:52 +0330

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+#Preconfig
+* rename config.php.example to config.php  
+* set your credentials in config.php
+* run `composer install` to install Radius plugin 
+
+#CONSTANTS
+CONSTANT    | VALUE
+----------- | ------------
+BOT_TOKEN   | Telegram Bot Token
+CHAT_ID     | ChannelID or CHAT ID of notification center
+SSH_HOST    | SSH Host to perform reboot command
+SSH_USERNAME    | SSH Username
+SSH_PASSWORD    | SSH Password
+FATAL_COUNT | FATAL failure number (perform ssh command)
+RADIUS_SERVER | Target Radius Server
+RADIUS_SECRET | Target Radius Secret
+ACCOUNTING_USERNAME | Username (basic access request)
+ACCOUNTING_PASSWORD | Password (basic access request)
+
+#Cron
+`5 * * * * /usr/bin/php /home/username/RasMon/main.php >/dev/null 2>&1`
+ 
---
+++ main.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'vendor/autoload.php';
+include('Net/SSH2.php');
+include('config.php');
+use Dapphp\Radius\Radius;
+
+function notify($msg){
+    file_get_contents("https://api.telegram.org/bot".BOT_TOKEN."/sendMessage?chat_id=". CHAT_ID ."&text=" . urlencode($msg));
+}
+function load(){
+    $last_state = 0;
+    if(file_exists('last_state')){
+        $last_state = (int) file_get_contents('last_state');
+    }
+    return $last_state;
+}
+function save($state){
+    file_put_contents("last_state",$state);
+}
+function perform_reboot(){
+    $ssh = new Net_SSH2(SSH_HOST);
+    if (!$ssh->login(SSH_USERNAME, SSH_PASSWORD)) {
+        notify("SSH FAILURE #WTF");
+        return;
+    }
+    echo $ssh->exec('reboot');
+    $times = load();
+    notify("Rebooting Server ...\nTotal Failures : ($times)");
+    save(0);
+}
+$client = new Radius();
+$client->setServer(RADIUS_SERVER)
+    ->setSecret(RADIUS_SECRET)
+    ->setNasPort(200200);
+$client->setMSChapPassword(ACCOUNTING_PASSWORD);
+$authenticated = $client->accessRequest(ACCOUNTING_USERNAME);
+
+if ($authenticated === false) {
+    $times = load();
+    $times++;
+    save($times);
+    notify("Server Is Down ($times)");
+    if($times >= FATAL_COUNT ){
+        perform_reboot();
+    }
+} else {
+    save(0);
+}
